b1c2f34cb6b040d485478f7a8e5a34f48d781e44
mrFranz17 <evgeniifzo@mail.ru>
2020-10-14 11:42:24 +0400

    main

--- README.py
+++ README.py
@@ -1,9 +1,10 @@
 # exemple_0
-repositoty for mr.franz
+#repositoty for mr.franz
 i = 0
 lst = []
 while i < 4:
-  n = imput('')
+  n = input('Insert for press : ')
   lst.append(n*2)
+  i = i + 1
 lst.sort()
 print(lst)
